f56f5d9e867db2ae777bca949ba0e1e3ddaac57f
Juanma Perales <sayantjm@gmail.com>
2020-11-22 18:54:13 +0100

    Adding comments

---
+++ README.md
@@ -0,0 +1,1 @@
+## Example of how to extend the drawing tools for Shapes 
--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,11 @@ import shapes.Shape;
 
 /**
  * Created by sayantjm on 21/11/20
+ * This class is extending the shape tool with more decorations.
+ * The jar Shape.2.0-SNAPSHOT has been added as dependency
+ * and in the package drawingtool a new Decorator (BlueBorderDrawing) class has been added
+ * It has been tested directly in the main as it is only an example of how it can be extended by
+ * others developers.
  */
 public class Main {
     public static void main(String... args) {
